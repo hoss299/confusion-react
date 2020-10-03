7cf17fee7c5b5f7332bc42738e64bec36769a61b
Hossam <hossamsaeedg99@yahoo.com>
2020-10-03 23:02:08 +0200

    components part1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+{
+  /* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+<p> Hello, I am Hossam ! </p>
+</header> */
+}
